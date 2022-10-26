b9c7923fc1c00051f9cfdd2028342042e68f3735
CTrierMaansson <ctm@clin.au.dk>
2022-10-26 16:27:47 +0200

    Code init

---
+++ Complete Anne T analysis.R
@@ -0,0 +1,199 @@
+library(DNAfusion)
+####Patient 1####
+#659
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val39.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-155.bam")
+EML4_ALK_analysis("PosDeduped-165.bam")
+
+####Patient 2####
+#A20
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val43.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-238.bam")
+
+####Patient 3####
+#4009
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val10.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4009-2.bam")
+EML4_ALK_analysis("PosDeduped-4009-3.bam")
+EML4_ALK_analysis("PosDeduped-4009-8.bam")
+EML4_ALK_analysis("PosDeduped-4009-14.bam")
+
+####Patient 4####
+#4014
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val32.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4014-2.bam")
+
+####Patient 5####
+#A7
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val20.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-44.bam")
+EML4_ALK_analysis("PosDeduped-88.bam")
+EML4_ALK_analysis("PosDeduped-10.bam")
+EML4_ALK_analysis("PosDeduped-116.bam")
+EML4_ALK_analysis("PosDeduped-281.bam")
+EML4_ALK_analysis("PosDeduped-252.bam")
+EML4_ALK_analysis("PosDeduped-314.bam")
+EML4_ALK_analysis("PosDeduped-296.bam")
+
+####Patient 6####
+#A10
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val30.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-67.bam")
+EML4_ALK_analysis("PosDeduped-125.bam")
+
+####Patient 7####
+#4011
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val47.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4011-2.bam")
+EML4_ALK_analysis("PosDeduped-4011-5.bam")
+EML4_ALK_analysis("PosDeduped-4011-11.bam")
+
+####Patient 8####
+#4012
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val14.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4012-2.bam")
+EML4_ALK_analysis("PosDeduped-4012-3.bam")
+
+
+####Patient 9####
+#4005
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val26.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4005-2.bam")
+EML4_ALK_analysis("PosDeduped-4005-3.bam")
+EML4_ALK_analysis("PosDeduped-4005-8.bam")
+EML4_ALK_analysis("PosDeduped-4005-14.bam")
+
+####Patient 10####
+#A16
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val01.bam")
+setwd("D:/Anne T samples")
+
+####Patient 11####
+#A4
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val33.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-204.bam")
+
+####Patient 12####
+#1895
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val21.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-36.bam")
+EML4_ALK_analysis("PosDeduped-41.bam")
+EML4_ALK_analysis("PosDeduped-81.bam")
+EML4_ALK_analysis("PosDeduped-98.bam")
+
+####Patient 13####
+#4015
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val25.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4015-2.bam")
+
+####Patient 14####
+#4006
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val37.bam")
+setwd("D:/Anne T samples")
+
+
+####Patient 15####
+#4008
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val18.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4008-11.bam")
+
+####Patient 16####
+#A17
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val16.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-168.bam")
+EML4_ALK_analysis("PosDeduped-862.bam")
+
+####Patient 17####
+#A18
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val15.bam")
+setwd("D:/Anne T samples")
+
+####Patient 18####
+#A8
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val09.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-61.bam")
+
+####Patient 19####
+#A9
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val34.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-74.bam")
+EML4_ALK_analysis("PosDeduped-110.bam")
+
+####Patient 20####
+#A13
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val38.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-112.bam")
+EML4_ALK_analysis("PosDeduped-257.bam")
+
+####Patient 21####
+#A19
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val02.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-220.bam")
+EML4_ALK_analysis("PosDeduped-239.bam")
+
+####Patient 22####
+#4004
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val42.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-4004-2.bam")
+EML4_ALK_analysis("PosDeduped-4004-3.bam")
+EML4_ALK_analysis("PosDeduped-4004-11.bam")
+
+
+####Patient 23####
+#A6
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val31.bam")
+setwd("D:/Anne T samples")
+EML4_ALK_analysis("PosDeduped-164.bam")
+EML4_ALK_analysis("PosDeduped-308.bam")
+
+####Patient 24####
+#4013
+setwd("D:/DNAfusion validation cohort")
+EML4_ALK_analysis("PosDeduped-val06.bam")
+
+BiocManager::install("GenVisR")
+library(GenVisR)
+
+
